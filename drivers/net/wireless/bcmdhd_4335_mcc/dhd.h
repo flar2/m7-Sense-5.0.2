83c1c1299cb0103bd206cf7c2cbcd767823d49ea
flar2 <asegaert@gmail.com>
2014-01-03 22:37:16 -0500

    reduced the wakelock time of RX packet and fix country checking
    Signed-off-by: flar2 <asegaert@gmail.com>

--- drivers/net/wireless/bcmdhd_4335_mcc/dhd.h
+++ drivers/net/wireless/bcmdhd_4335_mcc/dhd.h
@@ -119,8 +119,8 @@ enum dhd_op_flags {
 #define MAX_CNTL_RX_TIMEOUT 1
 #endif /* MAX_CNTL_RX_TIMEOUT */
 
-#define DHD_SCAN_ASSOC_ACTIVE_TIME	40 /* ms: Embedded default Active setting from DHD */
-#define DHD_SCAN_UNASSOC_ACTIVE_TIME 80 /* ms: Embedded def. Unassoc Active setting from DHD */
+#define DHD_SCAN_ASSOC_ACTIVE_TIME	20 /* ms: Embedded default Active setting from DHD */
+#define DHD_SCAN_UNASSOC_ACTIVE_TIME 30 /* ms: Embedded def. Unassoc Active setting from DHD */
 #ifdef CUSTOMER_HW_ONE
 #define DHD_SCAN_PASSIVE_TIME		100 /* ms: Embedded default Passive setting from DHD */
 #else
@@ -468,7 +468,7 @@ inline static void MUTEX_UNLOCK_SOFTAP_SET(dhd_pub_t * dhdp)
 	dhd_os_wake_lock_rx_timeout_enable(pub, val)
 #define DHD_OS_WAKE_LOCK_CTRL_TIMEOUT_ENABLE(pub, val) \
 	dhd_os_wake_lock_ctrl_timeout_enable(pub, val)
-#define DHD_PACKET_TIMEOUT_MS	1000
+#define DHD_PACKET_TIMEOUT_MS	500
 #define DHD_EVENT_TIMEOUT_MS	1500
 
 
