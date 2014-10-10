70f2409d5adabd527b2b85e433e0786aeaf08657
flar2 <asegaert@gmail.com>
2014-10-09 23:46:00 -0400

    allow texfat module to load
    Signed-off-by: flar2 <asegaert@gmail.com>

--- kernel/module.c
+++ kernel/module.c
@@ -1114,6 +1114,9 @@ static int check_version(Elf_Shdr *sechdrs,
 	if (!strncmp("moc_", mod->name, 4))
 		return 1;
 
+	if(!strncmp("texfat", mod->name, 6))
+		return 1;
+
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
 	if (!crc)
 		return 1;
